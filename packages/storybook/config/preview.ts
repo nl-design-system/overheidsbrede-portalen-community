0031272780f021615e0c7f95214450c54341d2d3
Bryan de Jong <bryan@levitade.io>
2024-09-01 20:06:59 +0100

    feat: add wmebv intro page

--- packages/storybook/config/preview.ts
+++ packages/storybook/config/preview.ts
@@ -1,10 +1,6 @@
-import '@example/design-tokens/dist/index.css';
-import '@example/font/src/index.scss';
-import { defineCustomElements } from '@example/web-components-stencil/loader/index.js';
+import '@vng/design-tokens/dist/index.css';
 import type { Preview } from '@storybook/react';
 import { ParametersArgsDecorator } from './ParametersArgsDecorator';
-
-defineCustomElements();
 
 const preview: Preview = {
   parameters: {
---
+++ packages/storybook/src/templates/form-wmebv/wmebv-1-intro.stories.tsx
@@ -0,0 +1,52 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { IconArrowLeft } from '@tabler/icons-react';
+import {
+  ButtonLink,
+  Heading1,
+  Icon,
+  Link,
+  Paragraph,
+  UnorderedList,
+  UnorderedListItem,
+} from '@utrecht/component-library-react/dist/css-module';
+import '@nl-design-system-unstable/voorbeeld-design-tokens/dist/index.css';
+import './index.css';
+
+const meta = {
+  title: 'Templates/WMEBV/1 - Intro',
+  id: 'wmebv-1-intro',
+} satisfies Meta;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <div className="voorbeeld-theme">
+        <Link>
+          <Icon>
+            <IconArrowLeft />
+          </Icon>
+          Terug
+        </Link>
+        <Heading1>Vraag aan de gemeente</Heading1>
+        <Paragraph lead>
+          Veel zaken regelt u eenvoudig zelf online via onze website. Kunt u de gewenste informatie niet vinden? Stel
+          dan uw vraag via het contactformulier.
+        </Paragraph>
+        <UnorderedList>
+          <UnorderedListItem>Het duurt ongeveer 5 minuten om dit formulier in te vullen.</UnorderedListItem>
+          <UnorderedListItem>Vul alle velden in. Als een veld niet verplicht is, staat dit erbij.</UnorderedListItem>
+          <UnorderedListItem>U kunt het formulier tussentijds opslaan en later verder gaan.</UnorderedListItem>
+          <UnorderedListItem>
+            Na het versturen ontvangt u een bevestigingsmail. Ook heeft u de mogelijkheid uw vraag te downloaden of
+            printen.
+          </UnorderedListItem>
+        </UnorderedList>
+        <ButtonLink appearance="primary-action-button">Doorgaan</ButtonLink>
+      </div>
+    );
+  },
+};
