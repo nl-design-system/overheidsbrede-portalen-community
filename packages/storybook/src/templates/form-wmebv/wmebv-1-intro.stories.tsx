58925c028a63ad9d497911f4a99aed61ebb1083e
Bryan de Jong <bryan@levitade.io>
2024-09-02 14:23:47 +0100

    feat: add den haag theme to wmebv page 1

--- packages/storybook/src/templates/form-wmebv/wmebv-1-intro.stories.tsx
+++ packages/storybook/src/templates/form-wmebv/wmebv-1-intro.stories.tsx
@@ -11,6 +11,7 @@ import {
   UnorderedListItem,
 } from '@utrecht/component-library-react/dist/css-module';
 import '@nl-design-system-unstable/voorbeeld-design-tokens/dist/index.css';
+import '@gemeente-denhaag/design-tokens-components/dist/theme/index.css';
 import './index.css';
 
 const meta = {
@@ -22,74 +23,42 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+const TemplatePage = ({ theme }: { theme: string }) => (
+  <>
+    <div className={`utrecht-document ${theme}`}>
+      <PageContent>
+        <main className={'utrecht-page-content__main'}>
+          <Link href="/#" className="voorbeeld-back-link">
+            <Icon>
+              <IconArrowLeft />
+            </Icon>
+            Terug
+          </Link>
+          <Heading1>Vraag aan de gemeente</Heading1>
+          <Paragraph lead>
+            Veel zaken regelt u eenvoudig zelf online via onze website. Kunt u de gewenste informatie niet vinden? Stel
+            dan uw vraag via het contactformulier.
+          </Paragraph>
+          <UnorderedList>
+            <UnorderedListItem>Het duurt ongeveer 5 minuten om dit formulier in te vullen.</UnorderedListItem>
+            <UnorderedListItem>Vul alle velden in. Als een veld niet verplicht is, staat dit erbij.</UnorderedListItem>
+            <UnorderedListItem>U kunt het formulier tussentijds opslaan en later verder gaan.</UnorderedListItem>
+            <UnorderedListItem>
+              Na het versturen ontvangt u een bevestigingsmail. Ook heeft u de mogelijkheid uw vraag te downloaden of
+              printen.
+            </UnorderedListItem>
+          </UnorderedList>
+          <ButtonLink appearance="primary-action-button">Doorgaan</ButtonLink>
+        </main>
+      </PageContent>
+    </div>
+  </>
+);
+
 export const Default: Story = {
-  render: () => {
-    return (
-      <div className="utrecht-document voorbeeld-theme">
-        <PageContent>
-          <main className={'utrecht-page-content__main'}>
-            <Link href="/#" className="voorbeeld-back-link">
-              <Icon>
-                <IconArrowLeft />
-              </Icon>
-              Terug
-            </Link>
-            <Heading1>Vraag aan de gemeente</Heading1>
-            <Paragraph lead>
-              Veel zaken regelt u eenvoudig zelf online via onze website. Kunt u de gewenste informatie niet vinden?
-              Stel dan uw vraag via het contactformulier.
-            </Paragraph>
-            <UnorderedList>
-              <UnorderedListItem>Het duurt ongeveer 5 minuten om dit formulier in te vullen.</UnorderedListItem>
-              <UnorderedListItem>
-                Vul alle velden in. Als een veld niet verplicht is, staat dit erbij.
-              </UnorderedListItem>
-              <UnorderedListItem>U kunt het formulier tussentijds opslaan en later verder gaan.</UnorderedListItem>
-              <UnorderedListItem>
-                Na het versturen ontvangt u een bevestigingsmail. Ook heeft u de mogelijkheid uw vraag te downloaden of
-                printen.
-              </UnorderedListItem>
-            </UnorderedList>
-            <ButtonLink appearance="primary-action-button">Doorgaan</ButtonLink>
-          </main>
-        </PageContent>
-      </div>
-    );
-  },
+  render: () => <TemplatePage theme={'voorbeeld-theme'} />,
 };
 
-export const DenHaagThema: Story = {
-  render: () => {
-    return (
-      <div className="utrecht-document denhaag-theme">
-        <PageContent>
-          <main className={'utrecht-page-content__main'}>
-            <Link href="/#" className="voorbeeld-back-link">
-              <Icon>
-                <IconArrowLeft />
-              </Icon>
-              Terug
-            </Link>
-            <Heading1>Vraag aan de gemeente</Heading1>
-            <Paragraph lead>
-              Veel zaken regelt u eenvoudig zelf online via onze website. Kunt u de gewenste informatie niet vinden?
-              Stel dan uw vraag via het contactformulier.
-            </Paragraph>
-            <UnorderedList>
-              <UnorderedListItem>Het duurt ongeveer 5 minuten om dit formulier in te vullen.</UnorderedListItem>
-              <UnorderedListItem>
-                Vul alle velden in. Als een veld niet verplicht is, staat dit erbij.
-              </UnorderedListItem>
-              <UnorderedListItem>U kunt het formulier tussentijds opslaan en later verder gaan.</UnorderedListItem>
-              <UnorderedListItem>
-                Na het versturen ontvangt u een bevestigingsmail. Ook heeft u de mogelijkheid uw vraag te downloaden of
-                printen.
-              </UnorderedListItem>
-            </UnorderedList>
-            <ButtonLink appearance="primary-action-button">Doorgaan</ButtonLink>
-          </main>
-        </PageContent>
-      </div>
-    );
-  },
+export const DenHaagTheme: Story = {
+  render: () => <TemplatePage theme={'denhaag-theme'} />,
 };
