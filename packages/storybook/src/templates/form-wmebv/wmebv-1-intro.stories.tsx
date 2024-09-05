1e15275cc870335dcdb94cd6a93be635c05625eb
Bryan de Jong <bryan@levitade.io>
2024-09-05 11:30:33 +0100

    feat: add header and footer

--- packages/storybook/src/templates/form-wmebv/wmebv-1-intro.stories.tsx
+++ packages/storybook/src/templates/form-wmebv/wmebv-1-intro.stories.tsx
@@ -3,9 +3,14 @@ import { IconArrowLeft } from '@tabler/icons-react';
 import {
   ButtonLink,
   Heading1,
+  Heading2,
   Icon,
   Link,
+  LinkList,
+  LinkListLink,
   PageContent,
+  PageFooter,
+  PageHeader,
   Paragraph,
   UnorderedList,
   UnorderedListItem,
@@ -13,10 +18,14 @@ import {
 import '@nl-design-system-unstable/voorbeeld-design-tokens/dist/index.css';
 import '@gemeente-denhaag/design-tokens-components/dist/theme/index.css';
 import './index.css';
+import { Logo, PageHeaderLogo } from './components/Logo';
 
 const meta = {
   title: 'Templates/WMEBV/1 - Intro',
   id: 'wmebv-1-intro',
+  parameters: {
+    layout: 'fullscreen',
+  },
 } satisfies Meta;
 
 export default meta;
@@ -26,7 +35,12 @@ type Story = StoryObj<typeof meta>;
 const TemplatePage = ({ theme }: { theme: string }) => (
   <>
     <div className={`utrecht-document ${theme}`}>
-      <PageContent>
+      <PageHeader className="voorbeeld-page-header">
+        <div className="todo-page-header__content">
+          <PageHeaderLogo />
+        </div>
+      </PageHeader>
+      <PageContent className="todo-page-content">
         <main className={'utrecht-page-content__main'}>
           <Link href="/#" className="voorbeeld-back-link">
             <Icon>
@@ -51,6 +65,34 @@ const TemplatePage = ({ theme }: { theme: string }) => (
           <ButtonLink appearance="primary-action-button">Doorgaan</ButtonLink>
         </main>
       </PageContent>
+      <PageFooter>
+        <div className="todo-page-footer__content">
+          <div className="todo-footer-content-group">
+            <div className="todo-footer-content-block">
+              <Logo />
+            </div>
+            <div className="todo-footer-content-block">
+              <Heading2>Contact</Heading2>
+              <address className="todo-address utrecht-paragraph">
+                Bel <Link href="tel:453453">453 453</Link> (maandag tot en met vrijdag van 09.00 tot 17.00 uur) of stuur
+                een e-mail naar{' '}
+                <Link href="mailto:vragen@gemeentevoorbeeld.nl">
+                  <span className="utrecht-url-data">vragen@gemeentevoorbeeld.nl</span>
+                </Link>
+                {'.'}
+              </address>
+            </div>
+            <div className="todo-footer-content-block">
+              <LinkList>
+                <LinkListLink href="/">Bescherming persoonsgegevens</LinkListLink>
+                <LinkListLink href="/">Gebruikersvoorwaarden</LinkListLink>
+                <LinkListLink href="/">Proclaimer</LinkListLink>
+                <LinkListLink href="/">Cookieverklaring</LinkListLink>
+              </LinkList>
+            </div>
+          </div>
+        </div>
+      </PageFooter>
     </div>
   </>
 );
