b89dcc90a093a01a8ee20bc0b2e79308000d9e5b
Bryan de Jong <bryan@levitade.io>
2025-07-24 14:34:24 +0200

    ci: add chromatic

--- packages/storybook/config/main.ts
+++ packages/storybook/config/main.ts
@@ -3,6 +3,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*stories.@(js|jsx|ts|tsx)', '../src/**/*.mdx'],
   addons: [
+    '@chromatic-com/storybook',
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
     '@storybook/addon-viewport',
