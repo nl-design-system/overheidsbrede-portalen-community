f1847641610dba1ab69c79a333398d7e2e850b40
Bryan de Jong <bryan@levitade.io>
2025-08-08 11:58:41 +0200

    feat: create basis documentation

---
+++ packages/storybook/src/templates/mijn-omgeving-basis/mijn-omgeving-basis.stories.tsx
@@ -0,0 +1,139 @@
+import '@amsterdam/design-system-css/dist/grid/grid.css';
+import { Grid } from '@amsterdam/design-system-react';
+import { Sidenav, SidenavItem, SidenavLink, SidenavList } from '@gemeente-denhaag/sidenav';
+import { Meta, StoryObj } from '@storybook/react';
+import '@nl-design-system-unstable/voorbeeld-design-tokens/dist/index.css';
+import '@gemeente-denhaag/design-tokens-components/dist/theme/index.css';
+import './index.css';
+import './tokens.css';
+import {
+  IconArchive,
+  IconBuildingCommunity,
+  IconChevronRight,
+  IconCurrencyEuro,
+  IconHome,
+  IconInbox,
+  IconLayoutGrid,
+  IconParking,
+  IconUser,
+} from '@tabler/icons-react';
+import {
+  BreadcrumbNav,
+  BreadcrumbNavLink,
+  BreadcrumbNavSeparator,
+  Icon,
+  Paragraph,
+} from '@utrecht/component-library-react/dist/css-module';
+import { ReactElement } from 'react';
+import { Layout } from '../../components/Layout';
+import { PageHeaderLogo, VoorbeeldFooterLogo } from '../../components/Logo';
+
+const meta = {
+  title: 'Templates/Mijn Omgeving/Basis/Pagina',
+  id: 'mijn-omgeving-basis',
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const TemplatePage = ({
+  logo,
+  theme,
+  footerLogo,
+}: {
+  logo: ReactElement;
+  theme: string;
+  footerLogo?: ReactElement;
+}) => (
+  <Layout logo={logo} className={theme} footerLogo={footerLogo}>
+    <BreadcrumbNav aria-labelledby="hidden-breadcrumb-header">
+      <h2 id="hidden-breadcrumb-header" hidden>
+        Kruimelpad
+      </h2>
+      <BreadcrumbNavLink href={'/#'}>Home</BreadcrumbNavLink>
+      <BreadcrumbNavSeparator>
+        <Icon>
+          <IconChevronRight />
+        </Icon>
+      </BreadcrumbNavSeparator>
+      <BreadcrumbNavLink href={'/#'} current disabled>
+        Gemeente Voorbeeld
+      </BreadcrumbNavLink>
+    </BreadcrumbNav>
+    <Grid>
+      <Grid.Cell span={4}>
+        <Sidenav>
+          <SidenavList>
+            <SidenavItem>
+              <SidenavLink href="/#" current>
+                <IconLayoutGrid />
+                Home
+              </SidenavLink>
+            </SidenavItem>
+          </SidenavList>
+          <SidenavList>
+            <SidenavItem>
+              <SidenavLink href="/#">
+                <IconInbox />
+                Berichten
+              </SidenavLink>
+            </SidenavItem>
+            <SidenavItem>
+              <SidenavLink href="/#">
+                <IconArchive />
+                Lopende zaken
+              </SidenavLink>
+            </SidenavItem>
+          </SidenavList>
+          <SidenavList>
+            <SidenavItem>
+              <SidenavLink href="/#">
+                <IconCurrencyEuro />
+                Belastingzaken
+              </SidenavLink>
+            </SidenavItem>
+            <SidenavItem>
+              <SidenavLink href="/#">
+                <IconHome />
+                WOZ
+              </SidenavLink>
+            </SidenavItem>
+            <SidenavItem>
+              <SidenavLink href="/#">
+                <IconParking />
+                Parkeren
+              </SidenavLink>
+            </SidenavItem>
+            <SidenavItem>
+              <SidenavLink href="/#">
+                <IconBuildingCommunity />
+                Erfpacht
+              </SidenavLink>
+            </SidenavItem>
+          </SidenavList>
+          <SidenavList>
+            <SidenavItem>
+              <SidenavLink href="/#">
+                <IconUser />
+                Gegevens
+              </SidenavLink>
+            </SidenavItem>
+          </SidenavList>
+        </Sidenav>
+      </Grid.Cell>
+      <Grid.Cell span={8}>
+        <Paragraph>Content area</Paragraph>
+      </Grid.Cell>
+    </Grid>
+  </Layout>
+);
+
+export const Default: Story = {
+  render: () => (
+    <TemplatePage logo={<PageHeaderLogo />} theme={'voorbeeld-theme'} footerLogo={<VoorbeeldFooterLogo />} />
+  ),
+};
